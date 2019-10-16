e7fbe2520128ccfb139a1a713b94ed7e2624c886
awaheed <awaheed@utexas.edu>
2019-10-16 15:59:04 -0500

    added basic styling

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const PORT=process.env.PORT||3000
 
 const app =express()
 
+
+// Middleware
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(express.static("public"))
+
+
+// handlebars
 app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine','handlebars')
 
 require('./routes/htmlRoutes')(app)
 
 app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}. Visit http://localhost:${PORT}`)
+    console.log(`Listening on port ${PORT}. Visit http://localhost:${PORT}/`)
 })
